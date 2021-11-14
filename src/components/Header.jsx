0edffa151f19f0819527b23763df64bbd6a3747d
Vijendra-Achar <vijendra.1995@gmail.com>
2021-11-14 20:03:59 +0530

    Search field is now implemented 🔍

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import AppLogo from '../assets/todo.svg';
 
 import './Header.scss';
 import '../scss/_utillities.scss';
+import '../scss/_material-button.scss';
 
 function Header() {
+  const [searchText, setSearchText] = useState('');
+
+  const searchFunc = (event) => {
+    setSearchText(event.target.value);
+  };
+
   return (
     <div className="header">
       <div className="container flex justify-space-between align-center">
+        {/* Left Side of he Header */}
         <div className="header-title flex justify-space-around align-center">
           <img className="header-logo" src={AppLogo} alt="" />
           <h3>MyTasks</h3>
         </div>
+
+        {/* Middle section of the header */}
+        <div className="header-search-field">
+          <input
+            className="header-search"
+            value={searchText}
+            onChange={(event) => searchFunc(event)}
+            placeholder="Search"
+            type="text"
+            name="search-box"
+            id="search-box"
+          />
+        </div>
+
+        {/* Right side of the header */}
         <div className="header-controls">
           <button className="button">Login</button>
         </div>
