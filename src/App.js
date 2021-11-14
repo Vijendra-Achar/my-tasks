2838371535b5fd027101aae445830eec6a5d175c
Vijendra-Achar <vijendra.1995@gmail.com>
2021-11-14 20:36:42 +0530

    React Rouder is now added 🧭

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import Header from './components/Header';
 
+import { Route, Routes } from 'react-router-dom';
+
+import Home from './pages/Home.jsx';
+
 import './App.scss';
 import './scss/_utillities.scss';
 
 function App() {
   return (
     <div>
+      {/* Main Navigation Header */}
       <Header />
-      <div className="App container">
-        <h1>New Todo App 🔥</h1>
-      </div>
+
+      {/* Router Paths */}
+      <Routes>
+        <Route path="/" exact element={<Home />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div>
+      <div className="App container">
+        <h1>New Todo App 🔥 - Home</h1>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
